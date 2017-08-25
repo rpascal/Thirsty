cb287c0bb0f4919e32bc97dc8bd688e2df8d99c1
rpascal <ryanpascal99@yahoo.com>
2017-08-24 22:15:13 -0400

    Init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {ENVIRONMENT} from "../../environments/environment.default"
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,7 +10,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    console.log(ENVIRONMENT.environment)
   }
 
 }
